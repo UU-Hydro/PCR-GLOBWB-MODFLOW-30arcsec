a3437961837eccf8aaf5aaa871a8318b55e73b8c
jarno.verkaik@deltares.nl <jarno.verkaik@deltares.nl>
2019-04-04 09:10:14 +0200

    Update.

--- fortran/mf6ggm/src/mf6ggm.f90
+++ fortran/mf6ggm/src/mf6ggm.f90
@@ -1,16 +1,19 @@
 program test
   ! modules
-  use mf6_module, only: tMf6
+  use mf6_module, only: tMf6, i4b
   
   implicit none
   
   type(tMf6), pointer :: ggm => null()
-  character(len=1024) :: f
+  integer(i4b) :: isol
+  character(len=1024) :: s, f, rootdir
 
-  call getarg(1,f)
+  call getarg(1,s); read(s,*) isol
+  call getarg(2,rootdir)
+  call getarg(3,f)
   
   allocate(ggm)
-  call ggm%init(f)
+  call ggm%init(isol, rootdir, f)
   call ggm%write()
   deallocate(ggm)
   
