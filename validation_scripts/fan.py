a17dddb7ba0c51c3ee382cbcaf713f10f53b5c15
verkaik <jarno.verkaik@deltares.nl>
2020-12-17 09:17:45 +0100

    Minor update.

--- validation_scripts/fan.py
+++ validation_scripts/fan.py
@@ -1,7 +1,10 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib.ticker as ticker
 import os.path
+from collections import OrderedDict as od
 from scipy.stats import skew as sp_skew
+
 
 def plot_hist(plt, isp, cont, fp, binList, bins, rgbList):
     print('Plotting for %s...'%cont)
@@ -16,7 +19,7 @@ def plot_hist(plt, isp, cont, fp, binList, bins, rgbList):
 
     datf = np.concatenate(dat, axis=0 )
     n = len(datf); mean = np.mean(datf); median = np.median(datf); stdev= np.std(datf); skew = sp_skew(datf)
-    s = '\n'.join(['n = %i'%n, 'mean = %.2f'%mean, 'median = %.2f'%median, 'stdev = %.2f'%stdev, 'skew = %.2f'%skew])
+    s = '\n'.join(['n =  %.2f [M]'%(float(n)/1e6), 'mean = %.2f'%mean, 'median = %.2f'%median, 'stdev = %.2f'%stdev, 'skew = %.2f'%skew])
 
     colList = []
     for rgb in rgbList:
@@ -25,6 +28,12 @@ def plot_hist(plt, isp, cont, fp, binList, bins, rgbList):
     #Stack the data
     ax = plt.subplot(2,3,isp)
     ax.set_title(cont)
+
+    scale_y = 1e6
+    ticks_y = ticker.FuncFormatter(lambda x, pos: '{0:g}'.format(x/scale_y))
+    ax.yaxis.set_major_formatter(ticks_y)
+    ax.set_xlabel('Residual $d_{obs} - d_{mod}$ [m]')
+    ax.set_ylabel('Count (M)')
 
     #n, bins, patches = plt.hist(dat, bins, stacked=True, color=colList, density=False, rwidth=0.9)
     n, bins, patches = plt.hist(dat, bins, stacked=True, color=colList, density=False)
@@ -38,7 +47,13 @@ if __name__ == "__main__":
     fp = r'f:\models\pcr-globwb-30arcsec\model_new\results_cartesius\steady_state\post\%s.wtd.stat.ss.19580101'
     ffig = 'ss-wtd-res'
 
-    contList = ['Europe', 'Asia', 'Africa', 'North-America', 'South-America', 'Australia-Oceania']
+    binDict = od()
+    binDict['Europe']            = np.linspace(-500., 100, 51,endpoint=False)
+    binDict['Asia']              = np.linspace(-500., 100, 51,endpoint=False)
+    binDict['Africa']            = np.linspace(-1500.,100,160,endpoint=False)
+    binDict['North-America']     = np.linspace(-1000.,100,100,endpoint=False)
+    binDict['South-America']     = np.linspace(-1000.,100,110,endpoint=False)
+    binDict['Australia-Oceania'] = np.linspace(-1000.,100,110,endpoint=False)
 
     binList = ['le-0.25',     '0.25-0.50',   '0.50-2.50',   '2.50-5.00',    '5.00-10.00', \
                '10.00-20.00', '20.00-40.00', '40.00-80.00', '80.00-160.00', 'gt-160.00']
@@ -47,13 +62,12 @@ if __name__ == "__main__":
                [224,243,249], [168,218,231], [113,173,210], [ 68,117,182], [ 49, 54, 148]]
     rgbList = rgbList[::-1]
 
-    bins = np.linspace(-1000.,100,110,endpoint=False)
 
     isp = 0
-    for cont in contList:
+    for cont in binDict.keys():
         isp += 1
-        plt = plot_hist(plt, isp, cont, fp, binList, bins, rgbList)
-    plt.gcf().suptitle('Water-depth residuals $d_{obs} - d_{mod}$ in meters',fontsize=20)
+        plt = plot_hist(plt, isp, cont, fp, binList, binDict[cont], rgbList)
+    plt.gcf().suptitle('Water-depth residuals $d_{obs} - d_{mod}$ [m]',fontsize=20)
 
     manager = plt.get_current_fig_manager()
     manager.window.showMaximized()
