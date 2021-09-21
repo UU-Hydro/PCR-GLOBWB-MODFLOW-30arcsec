362e2d1382dd3214aee2a4b81f97080a70a5be7a
verkaik <jarno.verkaik@deltares.nl>
2021-09-21 16:48:38 +0200

    Update EHDR library

--- fortran/ehdrlib/src/ehdr_test.f90
+++ fortran/ehdrlib/src/ehdr_test.f90
@@ -6,12 +6,18 @@ program ehdr_test
   !
   type(tEhdr), pointer :: ehdr => null()
   character(len=mxslen) :: fp, s
+  integer(i4b) :: nc, nr
   real(r4b) :: mv
-  real(r4b), dimension(:,:), pointer :: x
+  real(r4b), dimension(:,:), allocatable :: x
   real(r8b) :: xll, yll, cs
 ! ******************************************************************************  
   !
   fp = 's35e030_catch'
+  write(*,*) (mod(1.d0,1.d0).eq.0.d0)
+  !
+  ! read and scale
+  nc = 10; nr = 20; allocate(x(nc,nr))
+  mv = 0.d0; xll = -2.d0; yll = 5000.d0; cs = 4.d0
   !
   allocate(ehdr)
   call ehdr%read_grid(fp, x, mv, xll, yll, cs)
